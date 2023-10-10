6b4cb5834410af97091adfa76292c5f769f74fd8
Patryk Ruczynski <ptrkk17@gmail.com>
2023-10-10 16:54:44 +0200

    First commit, adding first file.

---
+++ lab1/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// 1
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Witaj świecie, tu Patryk Ruczyński!");
+
+        // 2
+        String hello = "Witaj świecie, tu Patryk Ruczyński!";
+
+        System.out.println(hello);
+    }
+
+
+}
