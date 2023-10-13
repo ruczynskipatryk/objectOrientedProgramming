c90f5bf437a4736d03834af2dea4ba21daef2768
Patryk Ruczynski <ptrkk17@gmail.com>
2023-10-13 15:50:50 +0200

    Update

--- lab1/src/main/java/org/example/Main.java
+++ lab1/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 // 1
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Witaj świecie, tu Patryk Ruczyński!");
 
@@ -9,6 +11,13 @@ public class Main {
         String hello = "Witaj świecie, tu Patryk Ruczyński!";
 
         System.out.println(hello);
+
+        // 3
+        String day = "10.";
+        String month = "01.";
+        String year = "1998";
+
+        System.out.println("Urodziłem się " + day + month + year);
     }
 
 
